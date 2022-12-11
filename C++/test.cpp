7bc3e1bc16f818d10c8a220edba388b83258648a
DynoW <104089032+DynoW@users.noreply.github.com>
2022-12-11 10:49:42 +0200

    new file:   C++/Teme/10-12-2022/ex-2/ex-2.txt 	modified:   C++/test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main(){
     int x;
-    fstream f("test.txt");
-    while (!f.eof()){
-        f>>x;
-        cout<<x;
+    cin >> x;
+    if(x>99){
+        cout << "Da";
+    } else {
+        cout << "Nu";
     }
     return 0;
 }

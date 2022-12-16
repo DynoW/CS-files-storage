7b635ab47515b7e0e685c11adb38ce7234657d14
DynoW <104089032+DynoW@users.noreply.github.com>
2022-12-16 11:40:39 +0200

    test

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <fstream>
 
 using namespace std;
 
 int main(){
-    int n;
-    ifstream f("ex.in");
-    ofstream g("ex.out");
-    
+    int n,d,p,s=0;
+    cin >> n;
+    d=2;
+    while(n!=1){
+        p=0;
+        while(n%d==0){
+            p=p+1;
+            n=n/d;
+        }
+        if(p!=0){
+                s=s+d;
+        }
+    }
+    cout << s;
     return 0;
 }

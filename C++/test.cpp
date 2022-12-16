aa205f514aa13f7e1be170a7a21632eb394f6c8b
DynoW <104089032+DynoW@users.noreply.github.com>
2022-12-16 10:58:04 +0200

    new file:   C++/numere.txt     modified:   C++/test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
 #include <fstream>
 
-using namespace std;
+using namespace std; /*
+Pe prima linie a fişierului numere.txt sunt memorate cel mult 90 de numere întregi cu cel mult două cifre fiecare, separate prin câte un spaţiu.
+Scrieţi programul C/C++ care să determine şi să afişeze pe ecran, media aritmetică a numerelor strict pozitive din fişier.
+Dacă fişierul nu conţine numere strict pozitive se afişează pe ecran mesajul NU EXISTA.
+Exemplu: dacă fişierul numere.txt conţine numerele 6 -26 0 9 -7 se va afişa
+valoarea 7.5.*/
 
 int main(){
-    int a,b = 0; 
-    int i;
-    for (i=0;i<10;i++){ b = a; a = i; } 
-    cout<<b;
+    int n,nr=1,s=0;
+    ifstream f("numere.txt");
+    while(f>>n)
+    {
+     if(n>0)
+       {if(n<=99) {s=s+n;
+          nr++;}}
+    }
+    nr--;
+    cout << s << " " << nr << endl;
+    float ma = float(s)/nr;
+    cout<<ma;
     return 0;
 }

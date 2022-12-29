072ba6d13e5a1ad090a3d738202a4a0af46fe3e7
DynoW <104089032+DynoW@users.noreply.github.com>
2022-12-29 20:04:01 +0000

    modified:   C++/test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -3,19 +3,6 @@
 using namespace std;
 
 int main(){
-    int n,d,p,s=0;
-    cin >> n;
-    d=2;
-    while(n!=1){
-        p=0;
-        while(n%d==0){
-            p=p+1;
-            n=n/d;
-        }
-        if(p!=0){
-                s=s+d;
-        }
-    }
-    cout << s;
+    cout << "Ntza";
     return 0;
 }

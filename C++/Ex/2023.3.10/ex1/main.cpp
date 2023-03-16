245fbf66627414a2f4692f5fc0ab2b993294843c
DynoW <104089032+DynoW@users.noreply.github.com>
2023-03-16 20:23:56 +0200

    Update main.cpp

--- C++/Ex/2023.3.10/ex1/main.cpp
+++ C++/Ex/2023.3.10/ex1/main.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n, i, j, v[100];
+    cin >> n;
+    for(i=0;i<n;i++){
+        cin >> v[i];
+        if(v[i]%5==0){
+            for(j=i;j<n-1;j++){
+                v[j]=v[j+1];
+            }
+            n--;
+            i--;
+        }
+    }
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(v[i]<v[j]){
+                swap(v[i],v[j]);
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        cout << v[i] << " ";
+    }
     return 0;
 }

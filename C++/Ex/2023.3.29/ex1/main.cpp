ec042d73422c2e061f596b19bcf69d7b0c7bd242
DynoW <104089032+DynoW@users.noreply.github.com>
2023-03-29 16:05:18 +0300

    Add files via upload

---
+++ C++/Ex/2023.3.29/ex1/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+/*se citeste un vector cu n elem, afis descresc elem vectorului in fct de nr cifrelor pare
+124 70 5842 13 6240*/
+
+int main()
+{
+    int n, v[100], i, j, v2[100], ok, cp;
+    cin >> n;
+    for(i=0;i<n;i++){
+        cin >> v[i];
+    }
+    for(i=0;i<n;i++){
+        j=0;
+        cp=v[i];
+        while(cp!=0){
+            if(cp%2==0){
+                j = j+1;
+            }
+            cp = cp/10;
+        }
+        v2[i] = j;
+
+    }
+    for(i=0;i<n;i++){
+        for(j=i;j<n+1;j++){
+            if(v2[i]<v2[j]){
+                swap(v[i],v[j]);
+                swap(v2[i],v2[j]);
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        cout << v[i] << " ";
+    }
+    return 0;
+}

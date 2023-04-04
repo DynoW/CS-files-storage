8c6eabe113b4f8e72d3841352f71362d7d91f100
DynoW <104089032+DynoW@users.noreply.github.com>
2023-04-04 19:55:52 +0300

    modified:   .gitignore 	modified:   C++/Ex/2023.4.4/test-alt/ex1/main.cpp

--- C++/Ex/2023.4.4/test-alt/ex1/main.cpp
+++ C++/Ex/2023.4.4/test-alt/ex1/main.cpp
@@ -5,10 +5,17 @@ using namespace std;
 int main(){
     int n, i, v[100], max=0;
     cin >> n;
-    for (i=0,i<n,i++){
+    for (i=0;i<n;i++){
         cin >> v[i];
     }
-    for(i=0,i<n,i++){
-        
+    for(i=0;i<n;i++){
+        if((v[i]%2==0)&&(v[i]>max)){
+            // Merge si cu 2 if-uri
+            max = v[i];
+        }
     }
+    for(i=0;i<n;i++){
+        cout << max;
+    }
+    return 0;
 }

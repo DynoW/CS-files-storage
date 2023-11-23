5a24411f289e91f4a5aa9c19a12334edb9371ecf
DynoW <104089032+DynoW@users.noreply.github.com>
2023-11-23 13:30:32 +0200

    modified:   XI/C++/2023.11.22/ex2/main.cpp

--- XI/C++/2023.11.22/ex2/main.cpp
+++ XI/C++/2023.11.22/ex2/main.cpp
@@ -18,25 +18,30 @@ calaure
 a
 */
 
-#include <iostream>
 #include <cstring>
+#include <iostream>
 
 using namespace std;
 
-int main()
-{
-    char c[21];
-    int i;
+int main() {
+    char c[21], cp[21];
+    int i, l;
     cin.get(c, 21);
+    strcpy(cp, c);
     cout << endl;
-    for(i=0;i<strlen(c);i++){
-        cout << c+i << endl;
+    for(i = 0; i < strlen(c); i++) {
+        cout << c + i << endl;
     }
     cout << endl;
-    int l=strlen(c);
-    for(i=0;i<=l;i++){
+    l = strlen(c);
+    for(i = 0; i < l; i++) {
         cout << c << endl;
-        c[strlen(c)-1]=NULL;
+        c[strlen(c) - 1] = NULL;
+    }
+    cout << endl;
+    for(i = 0; i <= l/2; i++) {
+        cout << cp + i << endl;
+        cp[strlen(cp) - 1] = NULL;
     }
     return 0;
 }

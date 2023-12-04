1e8fceea6913868aa31e32cd1f2a99e28e706955
Danutz <104089032+DynoW@users.noreply.github.com>
2023-12-04 08:15:46 +0200

    Add files via upload

---
+++ XI/C++/2023.12.04/ex6/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstream>
+
+using namespace std;
+
+int main()
+{
+    char s[256], sc[256];
+    char *p;
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        cin.get(s,256);
+        int len = strlen(s);
+    }
+    sc=strcpy(s)
+    return 0;
+}

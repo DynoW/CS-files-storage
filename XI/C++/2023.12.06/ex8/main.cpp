2bae1e7d1bde0e5653c8315ab19c599ab20f22c5
Danutz <104089032+DynoW@users.noreply.github.com>
2023-12-06 17:00:43 +0200

    Update main.cpp

--- XI/C++/2023.12.06/ex8/main.cpp
+++ XI/C++/2023.12.06/ex8/main.cpp
@@ -2,17 +2,33 @@
 #include <cstring>
 using namespace std;
 
-int main()
-{
+int main() {
     char q[20][21];
     int n, k, i;
     cin >> n >> k;
-    for(i=0;i<n;i++){
+
+    for (i = 0; i < n; i++) {
         cin >> q[i];
     }
-    for(i<0;i<n;i++){
-        if(strcmp(q[i]+strlen(q[i])-strlen(q[k]), q[k])==0)
-            cout << q[i] << " ";
+
+    char *suffix = q[k - 1];
+
+    int found = 0;
+    for (i = 0; i < n; i++) {
+        if (i != k - 1) {
+            int len1 = strlen(q[i]);
+            int len2 = strlen(suffix);
+
+            if (len1 >= len2 && strcmp(q[i] + len1 - len2, suffix) == 0) {
+                cout << q[i] << " ";
+                found = 1;
+            }
+        }
     }
+
+    if (!found) {
+        cout << "nu exista";
+    }
+
     return 0;
 }

fb958bda0423b5bd9b1a69e1ad629f78fd683aa8
DynoW <104089032+DynoW@users.noreply.github.com>
2023-12-11 17:43:33 +0200

    modified:   XI/C++/2023.12.06/ex8/main.cpp

--- XI/C++/2023.12.06/ex8/main.cpp
+++ XI/C++/2023.12.06/ex8/main.cpp
@@ -2,16 +2,16 @@
 #include <cstring>
 using namespace std;
 
-int main()
-{
+int main() {
     char q[20][21];
     int n, k, i;
     cin >> n >> k;
-    for(i=0;i<n;i++){
+    cin.get();
+    for(i = 0; i < n; i++) {
         cin >> q[i];
     }
-    for(i<0;i<n;i++){
-        if(strcmp(q[i]+strlen(q[i])-strlen(q[k]), q[k])==0)
+    for(i = 0; i < n; i++) {
+        if(strcmp(q[i] + strlen(q[i]) - strlen(q[k]), q[k]) == 0)
             cout << q[i] << " ";
     }
     return 0;

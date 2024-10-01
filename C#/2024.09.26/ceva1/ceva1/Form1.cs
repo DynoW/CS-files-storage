b824c620ef77c1d6c4e88194f983512bbd5f3870
Daniel Chirilă <104089032+DynoW@users.noreply.github.com>
2024-10-01 09:53:54 +0300

    Add files via upload

---
+++ C#/2024.09.26/ceva1/ceva1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ceva1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n, cmax=0;
+            n = int.Parse(textBox1.Text);
+            while(n!=0)
+            {
+                if (cmax < n % 10)
+                    cmax = n % 10;
+                n /= 10;
+            }
+            textBox2.Text = cmax.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n, cmin = 9;
+            n = int.Parse(textBox1.Text);
+            while (n != 0)
+            {
+                if (cmin > n % 10)
+                    cmin = n % 10;
+                n /= 10;
+            }
+            textBox3.Text = cmin.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = (int.Parse(textBox2.Text)+int.Parse(textBox3.Text)).ToString();
+        }
+    }
+}

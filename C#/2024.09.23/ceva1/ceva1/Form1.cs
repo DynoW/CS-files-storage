6d6062a22de36a4e01e3845a59584a333a841856
Daniel Chirilă <104089032+DynoW@users.noreply.github.com>
2024-10-01 09:52:37 +0300

    Add files via upload

---
+++ C#/2024.09.23/ceva1/ceva1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ceva1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, s;
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+            s = a + b;
+            textBox3.Text = s.ToString();
+
+        }
+    }
+}

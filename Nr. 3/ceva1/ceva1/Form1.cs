f82a3b0bc9839bdad09bfcba5692631fb453d603
Daniel Chirilă <104089032+DynoW@users.noreply.github.com>
2024-10-17 12:36:13 +0300

    Add files via upload

---
+++ Nr. 3/ceva1/ceva1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ceva1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form f, g, h;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            f = new Form2();
+            f.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g = new Form3();
+            g.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            h = new Form4();
+            h.ShowDialog();
+        }
+    }
+}
